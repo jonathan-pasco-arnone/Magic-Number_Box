3cc2f2a445b16260abe940cdaaf6800f4f8c69df
Ubuntu <ubuntu@ip-172-31-61-199.ec2.internal>
2023-02-23 22:03:30 +0000

    initial commit

---
+++ magic_square.py
@@ -0,0 +1,16 @@
+""" Generates all posible magic square combinations """
+#!/usr/bin/env python3
+
+# Created by: Jonathan Pasco-Arnone
+# Created on: February 2023
+
+MAGIC_NUMBER = 15
+
+def magic_square_generator(magic_square):
+    """ Generates all possible combinations of squares """
+    print("HELLO")
+    magic_square[2] = 5
+
+array_of_numbers = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+magic_square_generator(array_of_numbers)
