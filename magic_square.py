450c12ac421a62b6fdab855045005e7dd8bd4e94
Ubuntu <ubuntu@ip-172-31-61-199.ec2.internal>
2023-02-23 23:36:31 +0000

    new commit

--- magic_square.py
+++ magic_square.py
@@ -6,11 +6,55 @@
 
 MAGIC_NUMBER = 15
 
-def magic_square_generator(magic_square):
+def magic_square_generator(magic_square, digit):
     """ Generates all possible combinations of squares """
-    print("HELLO")
-    magic_square[2] = 5
+    # Checks if the square works, then prints it if it does
 
-array_of_numbers = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    if digit == 8:
+        while(magic_square[8] != 9):
+            if (square_checker(magic_square)):
+                print_square(magic_square)
+            magic_square[8] += 1
+            print_square(magic_square)
+    else:
+        for index in range(0,8,1):
+            if magic_square[digit + 1] == 9:
+                magic_square[digit] += 1
+                magic_square[digit + 1] = 0
+            magic_square_generator(magic_square, digit + 1)
 
-magic_square_generator(array_of_numbers)
+def print_square(magic_square):
+    """ Prints the magic square in proper format """
+    print("\n", magic_square[0], " ", magic_square[1], " ", magic_square[2], "\n\n",
+          magic_square[3], " ", magic_square[4], " ", magic_square[5], "\n\n",
+          magic_square[6], " ", magic_square[7], " ", magic_square[8], "\n")
+
+def square_checker(magic_square):
+    """ Checks if the magic square works """
+    rows = False
+    columns = False
+    diagonals = False
+
+    # Checks all rows
+    if (magic_square[0] + magic_square[1] + magic_square[2] == MAGIC_NUMBER and
+            magic_square[3] + magic_square[4] + magic_square[5] == MAGIC_NUMBER and
+            magic_square[6] + magic_square[7] + magic_square[8] == MAGIC_NUMBER):
+
+        rows = True
+
+    # Checks all columns
+    if (magic_square[0] + magic_square[3] + magic_square[6] == MAGIC_NUMBER and
+            magic_square[1] + magic_square[4] + magic_square[7] == MAGIC_NUMBER and
+            magic_square[2] + magic_square[5] + magic_square[8] == MAGIC_NUMBER):
+
+        columns = True
+
+    # Checks both diagonals
+    if (magic_square[0] + magic_square[4] + magic_square[8] == MAGIC_NUMBER and
+            magic_square[2] + magic_square[4] + magic_square[6] == MAGIC_NUMBER):
+
+        diagonals = True
+
+    return rows and columns and diagonals
+
+magic_square_generator([0, 0, 0, 0, 0, 0, 0, 0, 0], 0)
